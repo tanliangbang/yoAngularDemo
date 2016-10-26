ec83dd2bda325fefb6acf5d3abff7de0ab3f0308
tanliangbang <648103576@qq.com>
2016-10-26 16:04:25 +0800

    ='yeoman'

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reactTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the reactTestApp
+ */
+angular.module('reactTestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reactTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the reactTestApp
+ */
+angular.module('reactTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
